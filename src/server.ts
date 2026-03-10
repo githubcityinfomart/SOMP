8ccdacff70971c990d50569dac16234ba4120a98
githubcityinfomart <info@elitewebtechnologies.com>
2026-03-10 10:53:41 +0530

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,21 @@
-import dotenv from 'dotenv';
-import 'reflect-metadata';
-import { App } from './app';
-import { Config } from "./config/config";
+import dotenv from "dotenv";
+import "reflect-metadata";
+import { App } from "./app";
+
 dotenv.config();
 
-(async function (): Promise<void> {
-    try {
-        const uniconApp = new App();
-        const app = uniconApp.app;
-        await uniconApp.init();
-        const port = app.get("port") || 3000;
-        app.listen(port, '0.0.0.0', () => {
-            console.log("#######################################################");
-            console.log("#                                                     #");
-            console.log("#                                                     #");
-            console.log("# Routing Extenstion ", uniconApp.jsOrTs);
-            console.log("# Express server  Environment ", Config.getInstance().getEnvironment());
-            console.log("# Express server listening on port %s", port);
-            console.log("#                                                     #");
-            console.log("#                                                     #");
-            console.log("#######################################################");
+let appInstance: any;
 
-        });
-    } catch (error) {
-        console.error("Error starting the server:", error);
-    }
-})();
+async function startApp() {
+  if (!appInstance) {
+    const uniconApp = new App();
+    await uniconApp.init();
+    appInstance = uniconApp.app;
+  }
+  return appInstance;
+}
 
-
+export default async function handler(req: any, res: any) {
+  const app = await startApp();
+  return app(req, res);
+}
